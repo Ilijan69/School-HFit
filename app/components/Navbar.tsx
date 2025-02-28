1622c195c5aa073796c0498a6232369d3f55a4eb
Ilijan69 <fooltiktok06@gmail.com>
2025-02-28 12:16:35 +0200

    .

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,146 +1,131 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import Image from "next/image";
-import {
-  onAuthStateChanged,
-  User,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { FirebaseError } from "firebase/app";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { auth, db } from "../firebase/firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
-import { logout } from "../firebase/authService";
+import Link from "next/link"
+import Image from "next/image"
+import { onAuthStateChanged, type User, signInWithEmailAndPassword } from "firebase/auth"
+import { FirebaseError } from "firebase/app"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { auth, db } from "../firebase/firebaseConfig"
+import { doc, getDoc } from "firebase/firestore"
+import { logout } from "../firebase/authService"
 
 function NavBar() {
-  const [user, setUser] = useState<User | null>(null);
-  const [gender, setGender] = useState<"Male" | "Female" | null>(null);
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
-  const [accounts, setAccounts] = useState<{ email: string }[]>([]);
-  const [activeAccount, setActiveAccount] = useState<string | null>(null);
-  const [password, setPassword] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
-  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null)
+  const [gender, setGender] = useState<"Male" | "Female" | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [isPopupVisible, setIsPopupVisible] = useState(false)
+  const [accounts, setAccounts] = useState<{ email: string }[]>([])
+  const [activeAccount, setActiveAccount] = useState<string | null>(null)
+  const [password, setPassword] = useState<string>("")
+  const [errorMessage, setErrorMessage] = useState<string>("")
+  const router = useRouter()
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      setLoading(true)
       if (user) {
-        await user.reload();
-        setUser(user);
+        await user.reload()
+        setUser(user)
 
         try {
-          const previousAccounts = JSON.parse(
-            localStorage.getItem("accounts") || "[]"
-          );
-          const filteredAccounts = previousAccounts.filter(
-            (account: { email: string }) => account.email !== user.email
-          );
-          setAccounts(filteredAccounts);
+          const previousAccounts = JSON.parse(localStorage.getItem("accounts") || "[]")
+          const filteredAccounts = previousAccounts.filter((account: { email: string }) => account.email !== user.email)
+          setAccounts(filteredAccounts)
 
-          if (
-            !previousAccounts.some(
-              (account: { email: string }) => account.email === user.email
-            )
-          ) {
-            previousAccounts.push({ email: user.email });
-            localStorage.setItem("accounts", JSON.stringify(previousAccounts));
+          if (!previousAccounts.some((account: { email: string }) => account.email === user.email)) {
+            previousAccounts.push({ email: user.email })
+            localStorage.setItem("accounts", JSON.stringify(previousAccounts))
           }
         } catch (error) {
-          console.error("Error handling accounts in localStorage:", error);
+          console.error("Error handling accounts in localStorage:", error)
         }
 
         try {
-          const userDocRef = doc(db, "users", user.uid);
-          const userDoc = await getDoc(userDocRef);
+          const userDocRef = doc(db, "users", user.uid)
+          const userDoc = await getDoc(userDocRef)
           if (userDoc.exists()) {
-            setGender(userDoc.data().gender);
+            setGender(userDoc.data().gender)
           } else {
-            console.warn("User document does not exist");
+            console.warn("User document does not exist")
           }
         } catch (error) {
-          console.error("Error fetching user gender:", error);
+          console.error("Error fetching user gender:", error)
         }
       } else {
-        setUser(null);
-        setGender(null);
+        setUser(null)
+        setGender(null)
       }
-    });
+      setLoading(false)
+    })
 
-    return () => unsubscribe();
-  }, [user]); // Include 'user' in the dependency array
+    return () => unsubscribe()
+  }, [])
 
   const handleLogout = async () => {
-    await logout();
-    router.push("/");
-  };
+    await logout()
+    router.push("/")
+  }
 
   const handleSwitchAccount = async (email: string) => {
     if (!password) {
-      setErrorMessage("A password is needed!");
-      return;
+      setErrorMessage("Нужна е парола")
+      return
     }
 
     try {
-      await signInWithEmailAndPassword(auth, email, password);
+      await signInWithEmailAndPassword(auth, email, password)
       // Reset error message, but don't close the popup yet
-      await setErrorMessage("");
-      await setPassword("");
+      await setErrorMessage("")
+      await setPassword("")
       // Attempt to sign in with email and password
       // If sign-in is successful, close the popup and log out the previous user
-      await closePopup();
+      await closePopup()
 
-      router.push("/");
+      router.push("/")
     } catch (error: unknown) {
       if (error instanceof FirebaseError) {
         if (error.code === "auth/wrong-password") {
-          setErrorMessage("Incorrect password");
+          setErrorMessage("Неправилна парола")
         } else {
-          setErrorMessage("Incorrect password");
+          setErrorMessage("Неправилна парола")
         }
       } else if (error instanceof Error) {
-        setErrorMessage("An unexpected error occurred.");
+        setErrorMessage("An unexpected error occurred.")
       }
     }
-  };
+  }
 
   const handleRemoveAccount = (email: string) => {
-    const updatedAccounts = accounts.filter(
-      (account) => account.email !== email
-    );
-    setAccounts(updatedAccounts);
-    localStorage.setItem("accounts", JSON.stringify(updatedAccounts));
-  };
+    const updatedAccounts = accounts.filter((account) => account.email !== email)
+    setAccounts(updatedAccounts)
+    localStorage.setItem("accounts", JSON.stringify(updatedAccounts))
+  }
 
   const handleCancel = () => {
-    setActiveAccount(null);
-    setPassword("");
-    setErrorMessage(""); // Clear the error message when canceling
-  };
+    setActiveAccount(null)
+    setPassword("")
+    setErrorMessage("") // Clear the error message when canceling
+  }
 
   const togglePopup = () => {
-    setIsPopupVisible((prev) => !prev);
-  };
+    setIsPopupVisible((prev) => !prev)
+  }
 
   const closePopup = () => {
-    setPassword("");
-    setErrorMessage("");
-    setIsPopupVisible(false); // Close the main popup
-    setActiveAccount(null); // Close the password popup
-  };
+    setPassword("")
+    setErrorMessage("")
+    setIsPopupVisible(false) // Close the main popup
+    setActiveAccount(null) // Close the password popup
+  }
 
   return (
     <header>
       <nav className="navbar" onClick={closePopup}>
-        <Image
-          src="/Pics/HFit logo.png"
-          width={1000}
-          height={40}
-          className="logo"
-          alt="HF_logo"
-        />
-        {!user ? (
+        <Image src="/Pics/HFit logo.png" width={1000} height={40} className="logo" alt="HF_logo" />
+        {loading ? (
+          <div className="auth-container">{/* Show a subtle loading indicator or nothing while loading */}</div>
+        ) : !user ? (
           <div className="auth-container">
             <div className="buttons-container">
               <Link href="/register">
@@ -160,22 +145,10 @@ function NavBar() {
             <div className="avatar-container" onClick={togglePopup}>
               <p className="greeting">Здравей, {user.displayName}</p>
               {gender === "Male" && (
-                <Image
-                  src="/Pics/male_pfp.png"
-                  className="avatar"
-                  width={45}
-                  height={45}
-                  alt="Male Avatar"
-                />
+                <Image src="/Pics/male_pfp.png" className="avatar" width={45} height={45} alt="Male Avatar" />
               )}
               {gender === "Female" && (
-                <Image
-                  src="/Pics/femal_pfp.png"
-                  className="avatar"
-                  width={45}
-                  height={45}
-                  alt="Female Avatar"
-                />
+                <Image src="/Pics/femal_pfp.png" className="avatar" width={45} height={45} alt="Female Avatar" />
               )}
             </div>
             {isPopupVisible && (
@@ -189,67 +162,48 @@ function NavBar() {
                     {accounts.map((account, index) => (
                       <li
                         key={index}
-                        className={`account-item ${
-                          activeAccount === account.email ? "active" : ""
-                        }`}
+                        className={`account-item ${activeAccount === account.email ? "active" : ""}`}
                         onClick={() => {
                           if (activeAccount !== account.email) {
                             // Reset password and error message when selecting a different account
-                            setPassword("");
-                            setErrorMessage("");
+                            setPassword("")
+                            setErrorMessage("")
                           }
-                          setActiveAccount(
-                            activeAccount === account.email
-                              ? null
-                              : account.email
-                          );
+                          setActiveAccount(activeAccount === account.email ? null : account.email)
                         }}
                       >
                         <span>{account.email}</span>
                         <button
                           className="remove-account-button"
                           onClick={(e) => {
-                            e.stopPropagation();
-                            handleRemoveAccount(account.email);
+                            e.stopPropagation()
+                            handleRemoveAccount(account.email)
                           }}
                         >
                           ×
                         </button>
                         {activeAccount === account.email && (
-                          <div
-                            className="password-popup"
-                            onClick={(e) => e.stopPropagation()}
-                          >
-                            {errorMessage && (
-                              <p className="error-message">{errorMessage}</p>
-                            )}
+                          <div className="password-popup" onClick={(e) => e.stopPropagation()}>
+                            {errorMessage && <p className="error-message">{errorMessage}</p>}
                             <input
                               type="password"
                               value={password}
                               onChange={(e) => {
-                                setPassword(e.target.value);
-                                setErrorMessage("");
+                                setPassword(e.target.value)
+                                setErrorMessage("")
                               }}
                               placeholder="Password"
                               onKeyDown={(e) => {
                                 if (e.key === "Enter") {
-                                  handleSwitchAccount(account.email);
+                                  handleSwitchAccount(account.email)
                                 }
                               }}
                             />
                             <div className="popup-buttons">
-                              <button
-                                className="confirm-button"
-                                onClick={() =>
-                                  handleSwitchAccount(account.email)
-                                }
-                              >
+                              <button className="confirm-button" onClick={() => handleSwitchAccount(account.email)}>
                                 <span></span> Confirm
                               </button>
-                              <button
-                                className="cancel-button"
-                                onClick={handleCancel}
-                              >
+                              <button className="cancel-button" onClick={handleCancel}>
                                 <span></span> Cancel
                               </button>
                             </div>
@@ -281,7 +235,7 @@ function NavBar() {
             <li>
               <Link
                 href="/weight_progress"
-                className={user ? "active-link" : "inactive-link"}
+                className={loading ? "active-link" : user ? "active-link" : "inactive-link"}
               >
                 Теглови прогрес
               </Link>
@@ -289,7 +243,7 @@ function NavBar() {
             <li>
               <Link
                 href="/training_sessions"
-                className={user ? "active-link" : "inactive-link"}
+                className={loading ? "active-link" : user ? "active-link" : "inactive-link"}
               >
                 Тренировачни упражнения
               </Link>
@@ -298,7 +252,8 @@ function NavBar() {
         </div>
       </nav>
     </header>
-  );
+  )
 }
 
-export default NavBar;
+export default NavBar
+
