5cb92c06e9f4c36b9b491f5f16cd496e41884203
Ilijan69 <fooltiktok06@gmail.com>
2025-03-04 11:53:10 +0200

    next config change

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ['workoutimageshfit.s3.eu-north-1.amazonaws.com'], // Add your S3 hostname
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*', // Apply the headers to all routes
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
